833ada5ddd330feb1de0002863e976da71e09022
yutoVR <yuto.fushimi@icloud.com>
2024-04-27 01:35:08 +0900

    設定システム

---
+++ Assets/Scripts/Setting.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+
+public class Setting<T>
+{
+    readonly string key;
+
+    T value;
+    public T Value
+    {
+        get => value;
+        set
+        {
+            this.value = value;
+            Save();
+        }
+    }
+
+    readonly T defaultValue;
+
+    public Setting(string key, T defaultValue)
+    {
+        this.key = key;
+        this.defaultValue = defaultValue;
+
+        Load();
+    }
+
+    public static implicit operator T(Setting<T> setting) => setting.value;
+
+    static TypeCode TypeCode => Type.GetTypeCode(typeof(T));
+
+    void Load()
+    {
+        Value = TypeCode switch
+        {
+            TypeCode.String => (T)(object)PlayerPrefs.GetString(key, (string)(object)defaultValue),
+            _ => throw new InvalidOperationException($"未サポートの型が指定されました: {typeof(T)}"),
+        };
+    }
+
+    void Save()
+    {
+        switch (TypeCode)
+        {
+            case TypeCode.String:
+                PlayerPrefs.SetString(key, (string)(object)Value);
+                break;
+
+            default:
+                throw new InvalidOperationException($"未サポートの型が指定されました: {typeof(T)}");
+        }
+    }
+}
