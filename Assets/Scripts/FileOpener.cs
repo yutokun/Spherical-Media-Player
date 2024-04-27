2d21d06ef3911e0fc956c193167ca40ba09c35c1
yutoVR <yuto.fushimi@icloud.com>
2024-04-27 11:05:39 +0900

    ファイルを開くダイアログ

---
+++ Assets/Scripts/FileOpener.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using SFB;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FileOpener : MonoBehaviour
+{
+    [SerializeField]
+    Button openButton;
+
+    public event Action<string> OnOpen;
+
+    readonly ExtensionFilter[] extensionsFilter =
+    {
+        new("Movie Files", "mp4", "mov", "m4v"),
+        new("Photo Files", "png", "jpg", "jpeg"),
+    };
+
+    void Start()
+    {
+        openButton.onClick.AddListener(ShowOpenDialog);
+    }
+
+    void ShowOpenDialog()
+    {
+        var paths = StandaloneFileBrowser.OpenFilePanel("Open Movie...", Settings.LastPath, extensionsFilter, false);
+        var path = paths[0];
+        if (string.IsNullOrEmpty(path)) return;
+
+        Debug.Log($"開いたファイル: {path}");
+        Settings.LastPath.Value = Path.GetDirectoryName(path);
+    }
+}
--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -2,4 +2,5 @@ using System;
 
 public static class Settings
 {
+    public static Setting<string> LastPath { get; } = new("LastPath", Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
 }
